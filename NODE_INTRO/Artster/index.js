6b08f5ae316d89ec5bec766f863a435be81be2f4
seiji528 <oguraseiji528@icloud.com>
2025-06-30 01:12:56 +0900

    backend

---
+++ NODE_INTRO/Artster/index.js
@@ -0,0 +1,13 @@
+const giglet = require('figlet');
+const colors = require('colors');
+
+var figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data.green);
+});
